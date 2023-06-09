f49034b81292f11f403ca94acaf5be32ae6bb5a7
chyiyaqing <chyiyaqing@gmail.com>
2023-06-09 23:34:02 +0800

    array slice 用法

--- examples/go_demo/02-variables/main.go
+++ examples/go_demo/02-variables/main.go
@@ -29,6 +29,31 @@ func main() {
 	fmt.Println(runeArr[2], string(runeArr[2]))    // 35821 语
 	fmt.Println("len(runeArr): ", len(runeArr))    // len(runeArr):  4
 
+	// 数组array与切片slice
+	var arr = [5]int{1, 2, 3, 4, 5} // 一维
+	var arr2 [5][5]int              // 二维
+	for i := 0; i < len(arr); i++ {
+		arr[i] += 100
+	}
+	fmt.Println(arr)
+	fmt.Println(arr2)
+	// 切片是数组的抽象, 切片使用数组作为底层结构
+	// 切片包含三个组件: 容量，长度，指向底层数组的指针
+	slice1 := make([]float32, 0) // 长度为0的切片
+	fmt.Println(slice1)
+	slice2 := make([]float32, 3, 5) // [0,0,0] 长度为3容量为5的切片
+	fmt.Println(len(slice2), cap(slice2))
+	// 使用切片，添加元素，切片容量可以根据需要自动扩展
+	slice2 = append(slice2, 1, 2, 3, 4)
+	fmt.Println(len(slice2), cap(slice2))
+	// 子切片 [start, end]
+	sub1 := slice2[3:]
+	sub2 := slice2[:3]
+	sub3 := slice2[1:4]
+	fmt.Println(sub1, sub2, sub3)
+	// 合并切片
+	combined := append(sub1, sub2...) // sub2... 是切片解构的写法，将切片解构为N各独立的元素
+	fmt.Println(combined)
 }
 
 /*
